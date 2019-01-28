7cb1a268d8b02ec8dfe0994c9c048e6ba610996a
Hudson2D <s00883153@acad.tri-c.edu>
2019-01-27 19:14:30 -0500

    Final for Lab2

---
+++ Lab2/Lab2/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public string Index()
+        {
+            return "Product/Index is displayed";
+        }
+
+        public string Browse()
+        {
+            return "Browse displayed";
+        }
+
+        public string Details(int id)
+        {
+            return "Details displayed for ID = " + id;
+        }
+
+        public string Location(int zip)
+        {
+            return "Location displayed for zip = " + zip;
+        }
+    }
+}
