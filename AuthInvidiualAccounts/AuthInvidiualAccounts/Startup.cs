fad27ef1aae25e78e6801661a2d7c14bfd1a6790
Hudson2D <s00883153@acad.tri-c.edu>
2019-03-14 18:29:25 -0400

    Lab08 + Video

---
+++ AuthInvidiualAccounts/AuthInvidiualAccounts/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AuthInvidiualAccounts.Startup))]
+namespace AuthInvidiualAccounts
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
